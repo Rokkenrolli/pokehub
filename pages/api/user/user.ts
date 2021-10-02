74e9b2bd7fb3f3bdabe2ca11d929506504399742
Olli Oksanen <olli.oksanen15@gmail.com>
2021-10-02 21:41:31 +0300

    fix build error

--- pages/api/user/user.ts
+++ pages/api/user/user.ts
@@ -2,16 +2,3 @@
 
 import { useSession } from "next-auth/client";
 import prisma from "../../../lib/prisma";
-
-
-const addToFavourite = async (id:number) => {
-    const [session,loading] = useSession();
-    await prisma.user.update({
-        where: {
-           
-        },
-        data: {
-
-        }
-    }) 
-}
