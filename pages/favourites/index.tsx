0cb787c8d61765a1ec91e0f2740d69297cccf0ab
Olli <olli.e.oksanen@aalto.fi>
2021-10-08 11:49:20 +0300

    improve favouritepage errorhandling

--- pages/favourites/index.tsx
+++ pages/favourites/index.tsx
@@ -1,6 +1,7 @@
 import { Favourite } from ".prisma/client";
 import { GetServerSideProps, NextPage } from "next";
 import { getSession, useSession } from "next-auth/client";
+import Link from "next/link";
 import { PokemonIcon } from "../../components/pokemon/PokemonIcon";
 import styles from "../../styles/commons.module.css";
 import prisma from "./../../lib/prisma";
@@ -15,12 +16,26 @@ const FavoritePage: NextPage<Props> = ({ favourites }) => {
   return (
     <div className={styles["profile-container"]}>
       {session && <h1>Your favourites, {session.user?.name}</h1>}
-      {!session && <h1>Something went wrong</h1>}
-      <div style={{display:"grid",gridAutoFlow:"column",margin:"1em"}}>
-      {favourites.map((f, i) => {
-        return <PokemonIcon key={i} name={f.pokemonName} url={f.pokemonUrl}/>;
-      })}
-      {!favourites.length && <h3>You have no favourites yet, to favourite pokemon, please click the star</h3>}
+      {!session && (
+        <h1>
+          Please sign in to see your favourites, or go{" "}
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+        </h1>
+      )}
+      <div style={{ display: "grid", gridAutoFlow: "column", margin: "1em" }}>
+        {favourites.map((f, i) => {
+          return (
+            <PokemonIcon key={i} name={f.pokemonName} url={f.pokemonUrl} />
+          );
+        })}
+        {session && !favourites.length && (
+          <h3>
+            You have no favourites yet, to favourite pokemon, please click the
+            star
+          </h3>
+        )}
       </div>
     </div>
   );
